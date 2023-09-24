bb991973ed3ece741b14d2d5f4b4b0265372930b
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-09-24 14:37:22 +0200

    add(util/cli): implement get_cwd fn

--- src/util/cli.rs
+++ src/util/cli.rs
@@ -1,5 +1,8 @@
+use std::env;
+
 use crate::Error;
 
 pub fn get_cwd() -> Result<String, Error> {
-    todo!()
+    let working_dir = env::current_dir();
+    Ok(working_dir.map_err(|_| make_err!())?.to_str().unwrap().to_owned())
 }
