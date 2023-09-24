ec75f37ffaa11994b362bee8f888ec0da1872ea0
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-09-24 14:35:37 +0200

    add(lib): add Result wrapper for Error

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+use std::fmt;
 use std::io;
 
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[macro_export]
 macro_rules! make_err {
+    () => {
+        Error::default()
+    };
     ($t:ident, $v:literal) => {
         Error::$t(format!($v))
     };
@@ -15,6 +22,19 @@ pub enum Error {
   IO(String),
   #[default]
   Unexpected,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let msg = match self {
+            Error::NotFound(s) => format!("Not Found: {s}"),
+            Error::Missing(s) => format!("Missing: {s}"),
+            Error::Conflict(s) => format!("Conflict: {s}"),
+            Error::IO(s) => format!("IO: {s}"),
+            Error::Unexpected => format!("unexpected error"),
+        };
+        write!(f, "{}", msg)
+    }
 }
 
 impl From<io::Error> for Error {
