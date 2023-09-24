fa154def0196d16c467c08218e9f3f952c386a3c
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-09-24 14:43:18 +0200

    add(cli): add install cli fn

---
+++ src/cli/install.rs
@@ -0,0 +1,46 @@
+use crate::Error;
+use crate::exec;
+use crate::pkg::data::get_pkg;
+use crate::pkg::pkg::Pkg;
+use crate::util::{msg, path};
+
+pub fn install(pkg_name: &String) -> Result<(), Error> {
+    let p: Pkg = get_pkg(pkg_name)?;
+
+    start_install(p)?;
+
+    Ok(())
+}
+
+pub fn clone_pkg(p: &Pkg) -> Result<(), Error> {
+    if path::exists(&path::repo(&p.name)) {
+        // TODO: `--force` is not yet implemented
+        return Err(make_err!(Conflict, "repo already cloned. try again with --force."));
+    }
+
+    msg::clone(&p.name, &p.url);
+
+    exec::clone(&p.url, &p.name)?;
+
+    Ok(())
+}
+
+pub fn run_install(p: &Pkg) -> Result<(), Error> {
+    msg::build(&p.name, &path::repo(&p.name));
+
+    exec::run_in_repo(&p.name, p.install.clone())?;
+
+    Ok(())
+}
+
+pub fn start_install(p: Pkg) -> Result<(), Error> {
+    clone_pkg(&p)?;
+
+    run_install(&p)?;
+
+    p.pack()?;
+
+    p.install_pack()?;
+
+    Ok(())
+}
