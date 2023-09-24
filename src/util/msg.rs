e3b9f86b967519acfece07ac6312bf9f7e0a2040
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-09-24 14:36:49 +0200

    add(util/msg): add fetch and create_config messages

--- src/util/msg.rs
+++ src/util/msg.rs
@@ -1,15 +1,41 @@
-use super::colors::{COLOR_BLUE, COLOR_YELLOW, COLOR_RESET, COLOR_MAGENTA};
+use super::colors::{COLOR_BLUE, COLOR_CYAN, COLOR_MAGENTA, COLOR_RESET, COLOR_YELLOW};
 
 pub fn clone(name: &str, url: &str) {
-    println!("{}", format!(
-        "cloning {COLOR_MAGENTA}{}{COLOR_RESET} from {COLOR_YELLOW}{}{COLOR_RESET} ...",
-        name, url
-    ));
+    println!(
+        "{}",
+        format!(
+            "cloning {COLOR_MAGENTA}{}{COLOR_RESET} from {COLOR_YELLOW}{}{COLOR_RESET} ...",
+            name, url
+        )
+    );
 }
 
 pub fn build(name: &str, path: &str) {
-    println!("{}", format!(
-        "building {COLOR_MAGENTA}{}{COLOR_RESET} at {COLOR_BLUE}{}{COLOR_RESET} ...",
-        name, path
-    ));
+    println!(
+        "{}",
+        format!(
+            "building {COLOR_MAGENTA}{}{COLOR_RESET} at {COLOR_BLUE}{}{COLOR_RESET} ...",
+            name, path
+        )
+    );
+}
+
+pub fn fetch(name: &str, url: &str) {
+    println!(
+        "{}",
+        format!(
+            "fetching {COLOR_MAGENTA}{}{COLOR_RESET} from {COLOR_YELLOW}{}{COLOR_RESET} ...",
+            name, url
+        )
+    )
+}
+
+pub fn create_config(path: &str) {
+    println!(
+        "{}",
+        format!(
+            "creating config file at {COLOR_CYAN}{}{COLOR_RESET} ...",
+            path
+        )
+    )
 }
