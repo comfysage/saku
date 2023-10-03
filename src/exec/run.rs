689d618165f9106b85fdf1a3cf787c56e5d1a1f6
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-10-03 16:26:24 +0200

    fix(exec/run): fix command running; fix commmand output

--- src/exec/run.rs
+++ src/exec/run.rs
@@ -1,6 +1,39 @@
-use std::process::Stdio;
 use crate::Error;
-use std::process::Command;
+
+use std::io::{Read, stdout, Write, IoSlice};
+use std::process::{Command, Stdio};
+use std::thread;
+
+/// Pipe streams are blocking, we need separate threads to monitor them without blocking the primary thread.
+fn child_stream_to_vec<R>(mut stream: R) -> Result<(), Error>
+where
+    R: Read + Send + 'static,
+{
+    let out = stdout();
+    thread::Builder::new()
+        .name("child_stream_to_vec".into())
+        .spawn(move || loop {
+            let mut buf = [0];
+            match stream.read(&mut buf) {
+                Err(err) => {
+                    println!("{}] Error reading from stream: {}", line!(), err);
+                    break;
+                }
+                Ok(got) => {
+                    if got == 0 {
+                        break;
+                    } else if got == 1 {
+                        let value = buf[0];
+                        out.lock().write_vectored(&[IoSlice::new(&[value])]).unwrap();
+                    } else {
+                        println!("{}] Unexpected number of bytes: {}", line!(), got);
+                        break;
+                    }
+                }
+            }
+        })?;
+    Ok(())
+}
 
 pub fn run(cmd: Vec<String>, pwd: &str) -> Result<(), Error> {
     let mut cwd: String = pwd.to_string();
@@ -17,7 +50,10 @@ pub fn run(cmd: Vec<String>, pwd: &str) -> Result<(), Error> {
         cmd.arg("-c").arg(line);
         cmd.current_dir(cwd.as_str());
 
-        let _ = cmd.stdout(Stdio::piped()).stderr(Stdio::piped()).spawn()?;
+        let mut child = cmd.stdout(Stdio::piped()).stderr(Stdio::piped()).spawn()?;
+        child_stream_to_vec(child.stdout.take().expect("!stdout"))?;
+        child_stream_to_vec(child.stderr.take().expect("!stderr"))?;
+        child.wait()?;
     }
     Ok(())
 }
