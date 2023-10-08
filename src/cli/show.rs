20ecf7e471a55958a9bd7397c409bc7a865cabe1
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-10-08 10:29:48 +0200

    feat(cli/show): implement Pkg.show fn

--- src/cli/show.rs
+++ src/cli/show.rs
@@ -1,14 +1,33 @@
-use crate::Error;
-use crate::pkg::{pkg::Pkg, data::get_pkg};
+use crate::pkg::data::get_pkg;
+use crate::pkg::pkg::Pkg;
+use crate::util::{msg, url};
+use crate::Result;
 
-pub fn show(pkg_name: &String) -> Result<(), Error> {
+pub fn show(pkg_name: &String) -> Result<()> {
     let pkg = get_pkg(pkg_name)?;
-    pkg.show();
+    pkg.show()?;
     Ok(())
 }
 
 impl Pkg {
-    pub fn show(&self) {
-        todo!()
+    pub fn show(&self) -> Result<()> {
+        println!("{}", msg::general::present_name(&self.name, &self.group));
+
+        if self.desc.len() > 0 {
+            println!("{}", self.desc);
+        }
+        if self.url.len() > 0 {
+            println!(
+                "url  {}",
+                msg::general::url_f(&url::shorten_url(&self.url)?)
+            );
+        }
+        if self.install.len() > 0 {
+            println!("bash");
+            for s in &self.install {
+                println!("  {}", s);
+            }
+        }
+        Ok(())
     }
 }
