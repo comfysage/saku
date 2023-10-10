e0b6b8e0f5633124dcbbed415938f2a285b7db91
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-10-10 14:13:09 +0200

    fix(util/filepath): implement abs fn

--- src/util/filepath.rs
+++ src/util/filepath.rs
@@ -1,14 +1,44 @@
 use crate::Error;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-use super::cli::get_cwd;
+use super::cli::{get_cwd, self};
 
 pub fn join(path: &str, suffix: &str) -> String {
     Path::new(path).join(suffix).to_str().unwrap().to_string()
 }
 
 pub fn abs(path: &str) -> Result<String, Error> {
-    todo!()
+    let path = Path::new(path);
+    let mut components = path.components();
+
+    let mut normalized = if path.is_absolute() {
+        // "If a pathname begins with two successive <slash> characters, the
+        // first component following the leading <slash> characters may be
+        // interpreted in an implementation-defined manner, although more than
+        // two leading <slash> characters shall be treated as a single <slash>
+        // character."
+        if path.starts_with("//") && !path.starts_with("///") {
+            components.next();
+            PathBuf::from("//")
+        } else {
+            PathBuf::new()
+        }
+    } else {
+        PathBuf::from(cli::get_cwd()?)
+    };
+    normalized.extend(components);
+
+    // "Interfaces using pathname resolution may specify additional constraints
+    // when a pathname that does not name an existing directory contains at
+    // least one non- <slash> character and contains one or more trailing
+    // <slash> characters".
+    // A trailing <slash> is also meaningful if "a symbolic link is
+    // encountered during pathname resolution".
+    if path.ends_with("/") {
+        normalized.push("");
+    }
+
+    Ok(normalized.to_str().unwrap().to_string())
 }
 
 pub fn exists(path: &str) -> bool {
