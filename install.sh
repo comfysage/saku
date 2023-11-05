72ea15fca218963353b0e0922248258263039305
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-11-05 15:49:02 +0100

    feat(install.sh): add install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+printf " -\033[35m setting up\033[0m saku root\n"
+mkdir -p ~/.saku/repo
+cd ~/.saku/repo
+
+printf " -\033[35m cloning\033[0m saku from\033[33m https://github.com/crispybaccoon/saku\033[0m\n"
+git clone --filter=blob:none https://github.com/crispybaccoon/saku && cd saku
+
+printf " -\033[35m building\033[0m saku\n"
+cargo build -r
+
+printf " -\033[35m setting up\033[0m environment\n"
+./saku config init
+
+printf " -\033[35m finishing\033[0m installation\n"
+./saku task install saku
+
